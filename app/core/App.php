e7628c260b9bfd508800876f9db228854ee46bd5
fany.elek <fanyganesta@gmail.com>
2025-12-19 05:02:48 +0700

    MVC -> Persiapan Selesai, initial project dan initial commit

---
+++ app/core/App.php
@@ -0,0 +1,6 @@
+<?php
+    class App{
+        public function __CONSTRUCT(){
+            echo 'Echo dari App';
+        }
+    }
---
+++ app/init.php
@@ -0,0 +1,3 @@
+<?php
+    require 'core/App.php';
+    require 'core/Controller.php';
