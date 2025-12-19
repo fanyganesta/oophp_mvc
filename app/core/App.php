1bad12448f3a2bb5a177b65cb20bf09ea3c5c40e
fanyganesta <fanyganesta@gmail.com>
2025-12-19 09:49:51 +0700

    Controller sudah ditambahkan : :

--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,34 @@
 <?php
     class App{
+        protected $controller = 'home';
+        protected $method = 'index';
+        protected $params = [];
+
         public function __CONSTRUCT(){
             $url = $this->parseUrl();
-            var_dump($url);
+            
+            if( file_exists('../app/controllers/' . $url[0] . '.php')){
+                $this->controller = $url[0];
+            }
+            
+            require '../app/controllers/' . $this->controller . '.php';
+            $class = $this->controller;
+            $obj = new $class();
+
+            if( isset($url[1]) && method_exists($obj, $url[1] )){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+
+            if( !empty($url) ){
+                $this->params = array_values($url);
+            }
+
+            $method = $this->method;
+            $params = $this->params;
+            
+            call_user_func_array([new $class(), $method], $params);
+            
         }
 
         public function parseUrl(){
